e341d02376a6e66086888f05c51934db9aa42770
rg <raska.gergely@gmail.com>
2020-10-21 13:18:15 +0200

    :package: Add basic project structure

---
+++ app/Covid19Tracker/app/src/main/java/com/example/covid_19_tracker/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.covid_19_tracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+class MainActivity : AppCompatActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+	}
+
+}
