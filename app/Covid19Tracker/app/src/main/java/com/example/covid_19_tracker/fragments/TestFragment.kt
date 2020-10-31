030c53a29fad57c1a295f27d48791c1e0825cc0c
rg <raska.gergely@gmail.com>
2020-10-31 13:07:46 +0100

    :package: Add TestFragment.kt + layout

---
+++ app/Covid19Tracker/app/src/main/java/com/example/covid_19_tracker/fragments/TestFragment.kt
@@ -0,0 +1,17 @@
+package com.example.covid_19_tracker.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.covid_19_tracker.R
+
+class TestFragment : Fragment() {
+
+	override fun onCreateView(
+		inflater: LayoutInflater, container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View? = inflater.inflate(R.layout.fragment_test, container, false)
+
+}
