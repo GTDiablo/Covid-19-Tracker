c884f0d5fd045d30537ee207a18e4fc0f1d6814d
rg <raska.gergely@gmail.com>
2020-10-31 13:05:52 +0100

    :package: Add map fragment + layout

---
+++ app/Covid19Tracker/app/src/main/java/com/example/covid_19_tracker/fragments/MapFragment.kt
@@ -0,0 +1,17 @@
+package com.example.covid_19_tracker.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.covid_19_tracker.R
+
+class MapFragment : Fragment() {
+
+	override fun onCreateView(
+		inflater: LayoutInflater, container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View? = inflater.inflate(R.layout.fragment_map, container, false)
+
+}
