27ac9d2fb86bafc866a1ae890856ab8cb938257b
rg <raska.gergely@gmail.com>
2020-12-06 10:31:30 +0100

    :sparkles: Update CallBuilder.kt

--- app/Covid19Tracker/app/src/main/java/com/example/covid_19_tracker/api/CountryNamesApi.kt
+++ app/Covid19Tracker/app/src/main/java/com/example/covid_19_tracker/api/CountryNamesApi.kt
@@ -1,4 +1,12 @@
 package com.example.covid_19_tracker.api
 
+import com.example.covid_19_tracker.models.CountryNames
+import com.example.covid_19_tracker.models.CountryResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 interface CountryNamesApi {
+	@GET("countries")
+	fun getAllCountries(user: String?): Call<CountryNames>
 }
--- app/Covid19Tracker/app/src/main/java/com/example/covid_19_tracker/utils/CallBuilder.kt
+++ app/Covid19Tracker/app/src/main/java/com/example/covid_19_tracker/utils/CallBuilder.kt
@@ -2,17 +2,21 @@
 package com.example.fragments.movie.network.utils
 
 
+import com.example.covid_19_tracker.api.CountryNamesApi
 import com.example.covid_19_tracker.api.CountryResponseApi
 import com.example.covid_19_tracker.models.CountryResponse
 import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 open class CallBuilder() {
+	private val moshi: Moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
 	private fun retrofit() = Retrofit.Builder()
-		.baseUrl("https://api.example.com")
-		.addConverterFactory(MoshiConverterFactory.create())
+		.baseUrl("https://api.covid19api.com/")
+		.addConverterFactory(MoshiConverterFactory.create(moshi))
 		.build()
 
-	protected fun buildCountriesCall(): CountryResponseApi = retrofit().create(CountryResponseApi::class.java)
+	protected fun buildCountryNamesCall(): CountryNamesApi = retrofit().create(CountryNamesApi::class.java)
+	protected fun buildPopularCall(): CountryResponseApi = retrofit().create(CountryResponseApi::class.java)
 }
