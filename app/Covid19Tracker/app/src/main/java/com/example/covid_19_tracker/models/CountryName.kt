2fed18888868c9fd648368d1e543a142bef0bdee
rg <raska.gergely@gmail.com>
2020-12-06 10:26:32 +0100

    :package: Add CountryNamesApi.kt

--- app/Covid19Tracker/app/src/main/java/com/example/covid_19_tracker/models/CountryName.kt
+++ app/Covid19Tracker/app/src/main/java/com/example/covid_19_tracker/models/CountryName.kt
@@ -1,5 +1,14 @@
 package com.example.covid_19_tracker.models
 
-class CountryName {
+import com.squareup.moshi.Json
 
-}
+data class CountryNames(
+	val countries : List<CountryName>
+)
+
+data class CountryName(
+	@Json(name = "Country")
+	val country: String,
+	@Json(name = "Slug")
+	val slug: String,
+)
